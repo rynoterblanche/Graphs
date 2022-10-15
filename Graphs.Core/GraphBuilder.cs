37ec6da6da9f85101d6220280e2c159c47f72d42
rynoterblanche <ryno.terblanche@gmail.com>
2022-10-15 13:39:35 +0100

    move builder

--- Graphs.Core/GraphBuilder.cs
+++ Graphs.Core/GraphBuilder.cs
@@ -1,12 +1,10 @@
-﻿using Graphs.Core;
-
-namespace Graphs.Shared.UnitTests;
+﻿namespace Graphs.Core;
 
 public class GraphBuilder<T>
 {
     private readonly Graph<T> _graph = new();
 
-    public GraphBuilder<T> WithEdge(T fromValue, T toValue)
+    public GraphBuilder<T> WithDirectedEdge(T fromValue, T toValue)
     {
         var fromVertex = _graph.FirstOrDefault(v => v.Value.Equals(fromValue));
         if (fromVertex == null)
