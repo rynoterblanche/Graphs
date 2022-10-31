00c0395e95bee0d60d1b7b7d55ce6fd7e067c300
rynoterblanche <ryno.terblanche@gmail.com>
2022-10-31 18:43:16 +0000

    start refactoring sorters

--- src/Graphs.Infrastructure/Sorters/TopologicalSorterDfs.cs
+++ src/Graphs.Infrastructure/Sorters/TopologicalSorterDfs.cs
@@ -19,16 +19,15 @@ public class TopologicalSorterDfs<T> : IGraphSorter<T>
         return sorted;
     }
 
-    private void Visit(Vertex<T> vertex, List<Vertex<T>> sorted, Dictionary<Vertex<T>, bool> visited)
+    private void Visit(Vertex<T> vertex, ICollection<Vertex<T>> sorted, IDictionary<Vertex<T>, bool> visited)
     {
-        bool inProcess;
-        var alreadyVisited = visited.TryGetValue(vertex, out inProcess);
+        var alreadyVisited = visited.TryGetValue(vertex, out var inProcess);
 
         if (alreadyVisited)
         {
             if (inProcess)
             {
-                throw new CyclicGraphException<T>("Cannot sort graph - cyclic dependency found");
+                throw new CyclicGraphException("Cannot sort graph - cyclic dependency found");
             }
         }
         else
--- tests/Graphs.Infrastructure.UnitTests/TopologicalSortBfs.cs
+++ tests/Graphs.Infrastructure.UnitTests/TopologicalSortBfs.cs
@@ -25,7 +25,7 @@ public class TopologicalSortBfs
 
         void Action() => topologicalSorterBfs.Sort(graph);
 
-        Assert.Throws<CyclicGraphException<string>>(Action);
+        Assert.Throws<CyclicGraphException>(Action);
     }
 
 }
